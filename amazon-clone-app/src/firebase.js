e4402aad9062ab6d4d815b76279974fed4da5725
loh <lohit.se@gmail.com>
2020-10-27 12:49:52 -0400

    Header styling complete

---
+++ amazon-clone-app/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyArnWBZc0CYqTPxdpymCrA3Ij4DyEcGko8",
+    authDomain: "challenge-a7500.firebaseapp.com",
+    databaseURL: "https://challenge-a7500.firebaseio.com",
+    projectId: "challenge-a7500",
+    storageBucket: "challenge-a7500.appspot.com",
+    messagingSenderId: "335585055264",
+    appId: "1:335585055264:web:2a9e1d8af45bcde904a245",
+    measurementId: "G-VZG8N4W2TY"
+  };
