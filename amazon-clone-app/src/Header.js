9722895b3274b0e0a5f349bc2f76c92658767ae0
loh <lohit.se@gmail.com>
2020-10-27 21:32:25 -0400

    add to cart counter complete

--- amazon-clone-app/src/Header.js
+++ amazon-clone-app/src/Header.js
@@ -3,8 +3,11 @@ import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
-function header() {
+function Header() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="header">
       <Link to="/">
@@ -38,7 +41,9 @@ function header() {
         <Link to="/checkout">
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
-            <span className="header__optionLineTwo header__basketCount">0</span>
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
           </div>
         </Link>
       </div>
@@ -46,4 +51,4 @@ function header() {
   );
 }
 
-export default header;
+export default Header;
